e71774b07b84704690f9b4feb87b985a97a8a7a7
YSanchezAraujo <yoelsanchez006@gmail.com>
2020-05-21 13:00:44 -0400

    trying to debug on hals

--- algos/NMF.jl
+++ algos/NMF.jl
@@ -1,5 +1,12 @@
 using LinearAlgebra, Distributions, DSP;
 
+"""
+TODO: need to add convolutions, currently it's just vanilla NMF
+for learning / testing purposes. Once those check out next step is
+to add convolution, then implement seqCNMF with MU and HALS
+"""
+
+
 function matrix_distance(A::Array{Float64, 2}, B::Array{Float64, 2})
     UA, sA, VA = svd(A)
     UB, sB, VB = svd(B)
@@ -22,8 +29,26 @@ function F2norm(A::Array{Float64, 1}, B::Array{Float64, 1})
 end
 
 function mu_update(A, B, C)
+    # A is the to be updated matrix
     return A .* ((C*B') ./ (A*(B*B')) .+ eps())
 end
+
+# this doesn't really work
+function hals_update(A, B, C)
+    # A is the to be updated matrix
+    N, K = size(A)
+    colset = collect(1:K)
+    U = copy(A)
+    for l in 1:K
+	tsum = zeros(N)
+	for k in setdiff(colset, l)
+	    tsum .+= U[:, k]*(B[k, :]'*B[l, :])
+	end
+	U[:, l] = max.(0,  (C*B[l, :] - tsum) / (norm(B[l, :]) .+ eps()))
+    end
+    return U
+end
+
 
 """
 vanilla nmf with multiplicative updates
@@ -33,13 +58,13 @@ d: cost function
 tol: tolerance for the change in cost
 maxIter: maximum iteration to go up to
 """
-function nmf(X::Array{Float64, 2}, 
+function nmf_mu(X::Array{Float64, 2}, 
 	     k::Int64,
 	     d::Function; tol=1.0e-5, maxIter=1.0e6)
     
     N, T = size(X)
-    W = rand(Uniform(0.01, 0.05), (N, k))
-    H = rand(Uniform(0.01, 0.05), (k, T))
+    W = rand(Uniform(0.0, 1.0), (N, k))
+    H = rand(Uniform(0.0, 1.0), (k, T))
     curIter, deltaCost, F = 1.0, 1.0, 0.0
 
     while (deltaCost > tol) && (curIter < maxIter)
@@ -54,6 +79,41 @@ function nmf(X::Array{Float64, 2},
     return W, H, F, deltaCost, curIter
 
 end
+
+# doesn't really work b/c the updates seem to be off
+"""
+vanilla nmf with multiplicative updates
+X: input data
+k: rank of the approximating matrices
+d: cost function
+tol: tolerance for the change in cost
+maxIter: maximum iteration to go up to
+"""
+function nmf_hals(X::Array{Float64, 2},
+                  k::Int64,
+                  d::Function; tol=1.0e-5, maxIter=1.0e6)
+
+    N, T = size(X)
+    W = rand(Uniform(0.0, 1.0), (N, k))
+    H = rand(Uniform(0.0, 1.0), (k, T))
+    curIter, deltaCost, F = 1.0, 1.0, 0.0
+    W .*= dot(X, W*H) / dot(W*H, W*H)
+    H .*= dot(X*H', W) / dot(W'*W, H*H')
+
+    while (deltaCost > tol) && (curIter < maxIter)
+        W = hals_update(W, H, X)
+        H = hals_update(H', W', X')'
+        X_tild = W*H
+        F = d(X_tild, X)
+        deltaCost = abs(deltaCost - F)
+        curIter += 1
+    end
+    
+    return W, H, F, deltaCost, curIter
+
+end
+
+
 
 """
 indep_norm2(W[1, :], H, X, 1)
@@ -74,7 +134,7 @@ end
 function gradW(W, H, X)
     return (W'*W)*H - W'*X
 end
-#gradloss(w0, H, X, idx) = ForwardDiff.gradient(W_col -> indep_norm2(
+
 
 """
 vanilla nmf as above, but this time learning is done through gradient descent updates
@@ -94,25 +154,26 @@ function nmf_grad(X::Array{Float64, 2},
     curIter, deltaCost, F = 1.0, 1.0, 0.0
 
     while (deltaCost > tol) && (curIter < maxIter)
-	# unlike the above, this is not vertoized by matrix operations
-        # instead we do it explicitly iterating over each row
-
+        # TODO
     end
 
     return W, H, F, deltaCost, curIter
 
 end
 
+
 """
 X: input data
 k: number of factors
 l: factor duration
 λ: regularization strength
+tol: tolerance for the change in cost
+maxIter: maximum iteration to go up to
 """
-function fit_seqnmf(X::Array{Float64, 2}, 
-		    k::Int64, 
-		    l::Float64, 
-		    λ::Float64; tol=1.0e-5, maxIter=1.0e6)
+function seqnmf(X::Array{Float64, 2}, 
+		k::Int64, 
+		l::Float64, 
+		λ::Float64; tol=1.0e-5, maxIter=1.0e6)
 
     N, T = size(X)
     W = rand(Uniform(0.01, 0.05), (N, k))
@@ -120,6 +181,7 @@ function fit_seqnmf(X::Array{Float64, 2},
     curIter, deltaCost = 1, 1
 
     while (curIter < maxIter) && (deltaCost > tol)
+	# TODO
 	# update H with MU
 	# shift W and H to center W's in time
 	# renormalize W and H so rows of H have unit norm
